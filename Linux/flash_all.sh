28dcbfacbe18cda770df777416ee2948d11d0c60
HELLBOY017 <abhaygill017@gmail.com>
2023-12-29 03:20:24 +0530

    Introduce fastboot rom flashing scripts
    To be used for flashing the stock rom images generated by unpacking the payload.bin file in the full OTA zips provided by Nothing.
    The script is meant to be executed after the device has booted to the bootloader (or fastboot mode).
    
    Signed-off-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: spike0en <spike0en@users.noreply.github.com>
    Co-authored-by: PHATWalrus <PHATWalrus@users.noreply.github.com>
    Co-authored-by: Ali Shahawez Khan <viralbanda@users.noreply.github.com>

---
+++ Linux/flash_all.sh
@@ -0,0 +1,69 @@
+echo "########################################################"
+echo "#        Nothing Phone 2 Fastboot ROM Flasher V7       #"
+echo "#               Developed And Tested By                #"
+echo "#      Hellboy017, Ali Shahawez, Spike, Phatwalrus     #"
+echo "#         [Nothing Phone (2) Telegram Dev Team]        #"
+echo "#                Build Date:28/12/23                   #"
+echo "########################################################"
+
+echo "###############################"
+echo "#  CHANGING ACTIVE SLOT TO A  #"
+echo "###############################"
+fastboot --set-active=a
+
+echo "#######################"
+echo "#   FORMATTING DATA   #"
+echo "#######################"
+read -p "Wipe Data? (y/n) " DATA_RESP 
+case "$DATA_RESP" in
+    [yY] )
+        fastboot -w
+        ;;
+esac
+
+echo "##############################"
+echo "#   FLASHING BOOT/RECOVERY   #"
+echo "##############################"
+for i in boot vendor_boot dtbo recovery; do 
+    fastboot flash $i $i.img
+done
+
+echo "##########################"             
+echo "# REBOOTING TO FASTBOOTD #"       
+echo "##########################"
+fastboot reboot fastboot
+
+echo  "#######################"
+echo  "#  FLASHING FIRMWARE  #"
+echo  "#######################"
+for i in abl aop bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump; do
+    fastboot flash $i $i.img
+done
+
+echo "#################################"
+echo "#  RESIZING LOGICAL PARTITIONS  #"
+echo "#################################"
+for i in odm_a system_a system_ext_a product_a vendor_a vendor_dlkm_a odm_b system_b system_ext_b product_b vendor_b vendor_dlkm_b; do
+    fastboot delete-logical-partition $i-cow
+    fastboot delete-logical-partition $i
+    fastboot create-logical-partition $i 1
+done
+
+echo "###############################"
+echo "# FLASHING LOGICAL PARTITIONS #"
+echo "###############################"
+for i in system system_ext product vendor vendor_dlkm odm vbmeta vbmeta_system vbmeta_vendor; do
+    fastboot flash $i $i.img
+done
+
+echo "###############"
+echo "#  REBOOTING  #"
+echo "###############"
+read -p "Reboot to system? (y/n) " REBOOT_RESP 
+case "$REBOOT_RESP" in
+    [yY] )
+        fastboot reboot
+        ;;
+esac
+
+exit 1
