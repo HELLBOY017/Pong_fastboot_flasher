8c2204ddafa08cc6a4fae40aa80b8be7e98976e7
HELLBOY017 <abhaygill017@gmail.com>
2023-12-30 19:48:54 +0530

    Add a choice to flash firmware on both slots

--- Linux/flash_all.sh
+++ Linux/flash_all.sh
@@ -1,29 +1,28 @@
-echo "########################################################"
-echo "#        Nothing Phone 2 Fastboot ROM Flasher V7       #"
-echo "#               Developed And Tested By                #"
-echo "#      Hellboy017, Ali Shahawez, Spike, Phatwalrus     #"
-echo "#         [Nothing Phone (2) Telegram Dev Team]        #"
-echo "#                Build Date:28/12/23                   #"
-echo "########################################################"
+echo "#################################################"
+echo "#           Pong Fastboot ROM Flasher           #"
+echo "#              Developed/Tested By              #"
+echo "#  Hellboy017, Ali Shahawez, Spike, Phatwalrus  #"
+echo "#     [Nothing Phone (2) Telegram Dev Team]     #"
+echo "#################################################"
 
-echo "###############################"
-echo "#  CHANGING ACTIVE SLOT TO A  #"
-echo "###############################"
+echo "#############################"
+echo "# CHANGING ACTIVE SLOT TO A #"
+echo "#############################"
 fastboot --set-active=a
 
-echo "#######################"
-echo "#   FORMATTING DATA   #"
-echo "#######################"
+echo "###################"
+echo "# FORMATTING DATA #"
+echo "###################"
 read -p "Wipe Data? (y/n) " DATA_RESP 
-case "$DATA_RESP" in
+case $DATA_RESP in
     [yY] )
         fastboot -w
         ;;
 esac
 
-echo "##############################"
-echo "#   FLASHING BOOT/RECOVERY   #"
-echo "##############################"
+echo "##########################"
+echo "# FLASHING BOOT/RECOVERY #"
+echo "##########################"
 for i in boot vendor_boot dtbo recovery; do 
     fastboot flash $i $i.img
 done
@@ -33,16 +32,26 @@ echo "# REBOOTING TO FASTBOOTD #"
 echo "##########################"
 fastboot reboot fastboot
 
-echo  "#######################"
-echo  "#  FLASHING FIRMWARE  #"
-echo  "#######################"
-for i in abl aop bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump; do
-    fastboot flash $i $i.img
-done
+echo  "#####################"
+echo  "# FLASHING FIRMWARE #"
+echo  "#####################"
+read -p "Flash firmware on both slots? (y/n) " FIRMWARE_RESP 
+case $FIRMWARE_RESP in
+    [yY] )
+        for i in abl aop bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump; do
+            fastboot flash --slot=all $i $i.img
+        done
+        ;;
+    *)
+        for i in abl aop bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump; do
+            fastboot flash $i $i.img
+        done
+        ;;
+esac
 
-echo "#################################"
-echo "#  RESIZING LOGICAL PARTITIONS  #"
-echo "#################################"
+echo "###############################"
+echo "# RESIZING LOGICAL PARTITIONS #"
+echo "###############################"
 for i in odm_a system_a system_ext_a product_a vendor_a vendor_dlkm_a odm_b system_b system_ext_b product_b vendor_b vendor_dlkm_b; do
     fastboot delete-logical-partition $i-cow
     fastboot delete-logical-partition $i
@@ -56,11 +65,11 @@ for i in system system_ext product vendor vendor_dlkm odm vbmeta vbmeta_system v
     fastboot flash $i $i.img
 done
 
-echo "###############"
-echo "#  REBOOTING  #"
-echo "###############"
+echo "#############"
+echo "# REBOOTING #"
+echo "#############"
 read -p "Reboot to system? (y/n) " REBOOT_RESP 
-case "$REBOOT_RESP" in
+case $REBOOT_RESP in
     [yY] )
         fastboot reboot
         ;;
--- Windows/flash_all.bat
+++ Windows/flash_all.bat
@@ -1,49 +1,55 @@
 @echo off
 title Nothing Phone 2 Fastboot ROM Flasher (t.me/NothingPhone2)
 
-echo ########################################################
-echo #        Nothing Phone 2 Fastboot ROM Flasher V7       #
-echo #               Developed And Tested By                #
-echo #      Hellboy017, Ali Shahawez, Spike, Phatwalrus     #
-echo #         [Nothing Phone (2) Telegram Dev Team]        #
-echo #                Build Date:28/12/23                   #
-echo ########################################################
+echo #################################################
+echo #           Pong Fastboot ROM Flasher           #
+echo #              Developed/Tested By              #
+echo #  Hellboy017, Ali Shahawez, Spike, Phatwalrus  #
+echo #     [Nothing Phone (2) Telegram Dev Team]     #
+echo #################################################
 
-echo ###############################
-echo #  CHANGING ACTIVE SLOT TO A  #
-echo ###############################
+echo #############################
+echo # CHANGING ACTIVE SLOT TO A #
+echo #############################
 fastboot --set-active=a
 
-echo #######################
-echo #   FORMATTING DATA   #
-echo #######################
+echo ###################
+echo # FORMATTING DATA #
+echo ###################
 choice /m "Wipe Data?"
 if %errorlevel% equ 1 (
     fastboot -w
 )
 
-echo  ##############################
-echo  #   FLASHING BOOT/RECOVERY   #
-echo  ##############################
+echo ##########################
+echo # FLASHING BOOT/RECOVERY #
+echo ##########################
 for %%i in (boot vendor_boot dtbo recovery) do (
     fastboot flash %%i %%i.img
 )
 
-echo  ##########################             
-echo  # REBOOTING TO FASTBOOTD #       
-echo  ##########################
+echo ##########################             
+echo # REBOOTING TO FASTBOOTD #       
+echo ##########################
 fastboot reboot fastboot
 
-echo  #######################
-echo  #  FLASHING FIRMWARE  #
-echo  #######################
-for %%i in (abl aop bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump) do (
-    fastboot flash %%i %%i.img
+echo #####################
+echo # FLASHING FIRMWARE #
+echo #####################
+choice /m "Flash firmware on both slots?"
+if %errorlevel% equ 1 (
+    for %%i in (abl aop bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump) do (
+        fastboot flash --slot=all %%i %%i.img
+    )
+) else (
+    for %%i in (abl aop bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump) do (
+        fastboot flash %%i %%i.img
+    )
 )
 
-echo #################################
-echo #  RESIZING LOGICAL PARTITIONS  #
-echo #################################
+echo ###############################
+echo # RESIZING LOGICAL PARTITIONS #
+echo ###############################
 for %%i in (odm_a system_a system_ext_a product_a vendor_a vendor_dlkm_a odm_b system_b system_ext_b product_b vendor_b vendor_dlkm_b) do (
     fastboot delete-logical-partition %%i-cow
     fastboot delete-logical-partition %%i
@@ -57,9 +63,9 @@ for %%i in (system system_ext product vendor vendor_dlkm odm vbmeta vbmeta_syste
     fastboot flash %%i %%i.img
 )
 
-echo ###############
-echo #  REBOOTING  #
-echo ###############
+echo #############
+echo # REBOOTING #
+echo #############
 choice /m "Reboot to system?"
 if %errorlevel% equ 1 (
     fastboot reboot
