HELLBOY017/Pong_fastboot_flasher

commit 52748a2076904e84439b29f36f406ae214939eac

vbmeta partitions are not logical
Cosmetic change.

Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

Linux/flash_all.sh
@@ -46,7 +46,7 @@ case $FIRMWARE_RESP in
         ;;
 esac
 
-for i in abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump; do
+for i in abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multoem multqti qupfw qweslicstore shrm tz uefi uefisecapp vbmeta vbmeta_system vbmeta_vendor xbl xbl_config xbl_ramdump; do
     fastboot flash $SLOT $i $i.img
 done
 
@@ -62,7 +62,7 @@ done
 echo "###############################"
 echo "# FLASHING LOGICAL PARTITIONS #"
 echo "###############################"
-for i in system system_ext product vendor vendor_dlkm odm vbmeta vbmeta_system vbmeta_vendor; do
+for i in system system_ext product vendor vendor_dlkm odm; do
     fastboot flash $i $i.img
 done
 

Windows/flash_all.bat
@@ -45,7 +45,7 @@ if %errorlevel% equ 1 (
     set slot=--slot=a
 )
 
-for %%i in (abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump) do (
+for %%i in (abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multoem multqti qupfw qweslicstore shrm tz uefi uefisecapp vbmeta vbmeta_system vbmeta_vendor xbl xbl_config xbl_ramdump) do (
     fastboot flash %slot% %%i %%i.img
 )
 
@@ -61,7 +61,7 @@ for %%i in (odm_a system_a system_ext_a product_a vendor_a vendor_dlkm_a odm_b s
 echo ###############################
 echo # FLASHING LOGICAL PARTITIONS #
 echo ###############################
-for %%i in (system system_ext product vendor vendor_dlkm odm vbmeta vbmeta_system vbmeta_vendor) do (
+for %%i in (system system_ext product vendor vendor_dlkm odm) do (
     fastboot flash %%i %%i.img
 )
 

commit 4f04720bf5d54504a3318dae3d166708a7d3f153

Fix multiimg* image names
Previous commit wrongfully truncated multiimg to mult due to a sed
syntax error during list generation.

Fix it.

Fixes: 52748a207690 ("vbmeta partitions are not logical")
Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

Linux/flash_all.sh
@@ -46,7 +46,7 @@ sudo fastboot reboot fastboot
 echo  "#####################"
 echo  "# FLASHING FIRMWARE #"
 echo  "#####################"
-for i in abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multoem multqti qupfw qweslicstore shrm tz uefi uefisecapp vbmeta vbmeta_system vbmeta_vendor xbl xbl_config xbl_ramdump; do
+for i in abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp vbmeta vbmeta_system vbmeta_vendor xbl xbl_config xbl_ramdump; do
     sudo fastboot flash $SLOT $i $i.img
 done
 

Windows/flash_all.bat
@@ -45,7 +45,7 @@ fastboot reboot fastboot
 echo #####################
 echo # FLASHING FIRMWARE #
 echo #####################
-for %%i in (abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multoem multqti qupfw qweslicstore shrm tz uefi uefisecapp vbmeta vbmeta_system vbmeta_vendor xbl xbl_config xbl_ramdump) do (
+for %%i in (abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp vbmeta vbmeta_system vbmeta_vendor xbl xbl_config xbl_ramdump) do (
     fastboot flash %slot% %%i %%i.img
 )
 