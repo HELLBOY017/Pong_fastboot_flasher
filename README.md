62713054dd31c24fd910ad666f7f9f1ae079be9d
HELLBOY017 <abhaygill017@gmail.com>
2024-01-14 01:59:07 +0530

    Windows: Add ability to run as admin and error handling
    Also include platform tools

--- README.md
+++ README.md
@@ -15,6 +15,5 @@ chmod +x flash_all.sh && bash flash_all.sh
 ```
 
 ### Notes
-- The script should not be run as admin on Windows.
 - The script flashes the rom on slot A and it destroys the partitions on slot B to create space for the partitions which are being flashed on slot A. This is the reason why we are not including the ability to switch slots as the partitions would get destroyed on the inactive slot which is why the script flashes the partitions on the primary slot which is slot A.
 
--- Windows/flash_all.bat
+++ Windows/flash_all.bat
@@ -8,10 +8,24 @@ echo #  HELLBOY017, viralbanda, spike0en, PHATwalrus, arter97  #
 echo #          [Nothing Phone (2) Telegram Dev Team]          #
 echo ###########################################################
 
+cd %~dp0
+set fastboot=.\platform-tools\fastboot.exe
+
+if not exist %fastboot% (
+    echo Fastboot cannot be executed. Aborting
+    pause
+    exit
+)
+
 echo #############################
 echo # CHANGING ACTIVE SLOT TO A #
 echo #############################
-fastboot --set-active=a
+%fastboot% --set-active=a
+if %errorlevel% neq 0 (
+    echo Error occured while switching to slot A. Aborting
+    pause
+    exit
+)
 
 echo ###################
 echo # FORMATTING DATA #
@@ -19,10 +33,13 @@ echo ###################
 choice /m "Wipe Data?"
 if %errorlevel% equ 1 (
     echo Please ignore "Did you mean to format this partition?" warnings.
-    fastboot erase userdata
-    fastboot erase metadata
+    call :ErasePartition userdata
+    call :ErasePartition metadata
 )
 
+echo ##########################
+echo # FLASHING BOOT/RECOVERY #
+echo ##########################
 choice /m "Flash images on both slots? If unsure, say N."
 if %errorlevel% equ 1 (
     set slot=all
@@ -30,58 +47,62 @@ if %errorlevel% equ 1 (
     set slot=a
 )
 
-echo ##########################
-echo # FLASHING BOOT/RECOVERY #
-echo ##########################
 for %%i in (boot vendor_boot dtbo recovery) do (
     if %slot% equ all (
         for %%s in (a b) do (
-            fastboot flash %%i_%%s %%i.img
+            call :FlashImage %%i_%%s, %%i.img
         )
     ) else (
-        fastboot flash %%i %%i.img
+        call :FlashImage %%i, %%i.img
     )
 )
 
 echo ##########################             
 echo # REBOOTING TO FASTBOOTD #       
 echo ##########################
-fastboot reboot fastboot
+%fastboot% reboot fastboot
+if %errorlevel% neq 0 (
+    echo Error occured while rebooting to fastbootd. Aborting
+    pause
+    exit
+)
 
 echo #####################
 echo # FLASHING FIRMWARE #
 echo #####################
 for %%i in (abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump) do (
-    fastboot flash --slot=%slot% %%i %%i.img
+    call :FlashImage "--slot=%slot% %%i", %%i.img
 )
 
 echo ###################
 echo # FLASHING VBMETA #
 echo ###################
+set disable_avb=0
 choice /m "Disable android verified boot?, If unsure, say N. Bootloader won't be lockable if you select Y."
 if %errorlevel% equ 1 (
     set disable_avb=1
-    fastboot flash --slot=%slot% vbmeta --disable-verity --disable-verification vbmeta.img
+    call :FlashImage "--slot=%slot% vbmeta --disable-verity --disable-verification", vbmeta.img
 ) else (
-    fastboot flash --slot=%slot% vbmeta vbmeta.img
+    call :FlashImage "--slot=%slot% vbmeta", vbmeta.img
 )
 
-echo Flash logical partition images?
-echo If you're about to install a custom ROM that distributes its own logical partitions, say N.
-choice /m "If unsure, say Y."
-if %errorlevel% equ 1 (
+if not exist super.img (
     echo ###############################
     echo # FLASHING LOGICAL PARTITIONS #
     echo ###############################
     for %%i in (system system_ext product vendor vendor_dlkm odm) do (
         for %%s in (a b) do (
-            fastboot delete-logical-partition %%i_%%s-cow
-            fastboot delete-logical-partition %%i_%%s
-            fastboot create-logical-partition %%i_%%s 1
+            call :DeleteLogicalPartition %%i_%%s-cow
+            call :DeleteLogicalPartition %%i_%%s
+            call :CreateLogicalPartition %%i_%%s, 1
         )
-
-        fastboot flash %%i %%i.img
+        call :FlashImage %%i, %%i.img
     )
+) else (
+    echo ##################
+    echo # FLASHING SUPER #
+    echo ##################
+    call :FlashImage super, super.img
 )
 
 echo #################################
@@ -89,9 +110,9 @@ echo # FLASHING VBMETA SYSTEM/VENDOR #
 echo #################################
 for %%i in (vbmeta_system vbmeta_vendor) do (
     if %disable_avb% equ 1 (
-        fastboot flash %%i --disable-verity --disable-verification %%i.img
+        call :FlashImage "%%i --disable-verity --disable-verification", %%i.img
     ) else (
-        fastboot flash %%i %%i.img
+        call :FlashImage %%i, %%i.img
     )
 )
 
@@ -100,7 +121,7 @@ echo # REBOOTING #
 echo #############
 choice /m "Reboot to system? If unsure, say Y."
 if %errorlevel% equ 1 (
-    fastboot reboot
+    %fastboot% reboot
 )
 
 echo ########
@@ -110,3 +131,39 @@ echo Stock firmware restored.
 echo You may now optionally re-lock the bootloader if you haven't disabled android verified boot.
 
 pause
+exit
+
+:DeleteLogicalPartition
+%fastboot% delete-logical-partition %~1
+if %errorlevel% neq 0 (
+    call :Choice "Deleting %~1 partition failed"
+)
+exit /b
+
+:CreateLogicalPartition
+%fastboot% create-logical-partition %~1 %~2
+if %errorlevel% neq 0 (
+    call :Choice "Creating %~1 partition failed"
+)
+exit /b
+
+:ErasePartition
+%fastboot% erase %~1
+if %errorlevel% neq 0 (
+    call :Choice "Erasing %~1 partition failed"
+)
+exit /b
+
+:FlashImage
+%fastboot% flash %~1 %~2
+if %errorlevel% neq 0 (
+    call :Choice "Flashing %~2 failed"
+)
+exit /b
+
+:Choice
+choice /m "%~1 continue? If unsure say N"
+if %errorlevel% equ 2 (
+    exit
+)
+exit /b
