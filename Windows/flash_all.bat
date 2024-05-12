f074e52e52f7f3941a1ca8fd642177cfec4d46cb
Abhay Singh Gill <abhaygill017@gmail.com>
2024-05-12 18:38:19 +0530

    Revert "Replace VBScript with PowerShell for Zip Extraction"
    This reverts commit c6298bbd27a6518bca9e52d20ff95893784d4183.

--- Windows/flash_all.bat
+++ Windows/flash_all.bat
@@ -154,9 +154,19 @@ pause
 exit
 
 :UnZipFile
-if not exist "%~dp0platform-tools-latest" (
-    powershell -command "Expand-Archive -Path '%~dp0platform-tools-latest.zip' -DestinationPath '%~dp0platform-tools-latest' -Force"
-)
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists("%~1") Then
+>>%vbs% echo fso.CreateFolder("%~1")
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace("%~2").items
+>>%vbs% echo objShell.NameSpace("%~1").CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
 exit /b
 
 :ErasePartition
