04fa24f3d3e23c56a66fc4b26df323740cfaff04
HELLBOY017 <abhaygill017@gmail.com>
2026-09-19 15:36:58 +0530

    metroid

--- bash/flash_all.sh
+++ bash/flash_all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "#############################"
-echo "# Pong Fastboot ROM Flasher #"
-echo "#############################"
+echo "################################"
+echo "# Metroid Fastboot ROM Flasher #"
+echo "################################"
 
 ##----------------------------------------------------------##
 if [ ! -d "$(pwd)/platform-tools" ]; then
@@ -24,9 +24,9 @@ if [ ! -f "$fastboot" ] || [ ! -x "$fastboot" ]; then
 fi
 
 # Partition Variables
-boot_partitions="boot vendor_boot dtbo recovery"
-firmware_partitions="abl aop aop_config bluetooth cpucp devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti qupfw qweslicstore shrm tz uefi uefisecapp xbl xbl_config xbl_ramdump"
-logical_partitions="system system_ext product vendor vendor_dlkm odm"
+boot_partitions="boot init_boot vendor_boot dtbo recovery"
+firmware_partitions="abl aop aop_config bluetooth cpucp cpucp_dtb devcfg dsp featenabler hyp imagefv keymaster modem multiimgoem multiimgqti pvmfw qupfw shrm soccp_dcd soccp_debug tz uefi uefisecapp xbl xbl_config xbl_ramdump"
+logical_partitions="system system_dlkm system_ext product vendor vendor_dlkm odm"
 junk_logical_partitions="null"
 
 if [ -f super.img ]; then
